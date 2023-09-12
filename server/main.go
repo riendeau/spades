f261a3a5959c0c1ec02949b4547de6ef42e9d2a5
John Riendeau <jriendeau@gmail.com>
2023-09-11 22:57:36 -0500

    Draw a card

--- server/main.go
+++ server/main.go
@@ -6,65 +6,98 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
 
-type player struct {
-	token        string
-	name         string
+type client struct {
 	eventChannel chan string
+	name         string
+	seat         int
 }
 
-var players []player
+var clients []*client
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
-func join(conn *websocket.Conn, name string) {
-	log.Printf("Received register request; name: %s", name)
-	if len(players) >= 4 {
-		sendMessage(conn, "joinreply full")
-		return
-	}
-
-	newPlayer := player{
-		name:         name,
-		token:        uuid.NewString(),
-		eventChannel: make(chan string),
-	}
-
-	go func() {
-		for nextEvent := range newPlayer.eventChannel {
-			sendMessage(conn, nextEvent)
+func getPlayers() [4]*client {
+	var playerClients [4]*client
+	for i := range clients {
+		if clients[i].seat > 0 {
+			playerClients[clients[i].seat-1] = clients[i]
 		}
-	}()
+	}
+	return [4]*client(playerClients)
+}
 
-	players = append(players, newPlayer)
+func freeIdx() int {
+	curPlayers := getPlayers()
+	var freeIdx int
+	for freeIdx = 0; freeIdx < 4; freeIdx++ {
+		if curPlayers[freeIdx] == nil {
+			break
+		}
+	}
+	return freeIdx
+}
 
-	joinReply := "players"
-	for _, player := range players {
-		joinReply = fmt.Sprintf("%s %s", joinReply, player.name)
+func sit(client *client, name string) {
+	log.Printf("Received sit request; name: %s", name)
+	client.name = name
+
+	if client.seat == 0 {
+		freeIdx := freeIdx()
+		if freeIdx > 3 {
+			client.eventChannel <- "joinreply full"
+			return
+		}
+
+		client.seat = freeIdx + 1
+		log.Printf("Seated %s in seat %d", name, client.seat)
 	}
 
+	newPlayerList := buildPlayerList()
+	for i := range clients {
+		clients[i].eventChannel <- newPlayerList
+	}
+
+	if freeIdx := freeIdx(); freeIdx > 3 {
+		for i := range clients {
+			clients[i].eventChannel <- "newhand"
+		}
+	}
+}
+
+func buildPlayerList() string {
+	playerListReply := "players"
+	players := getPlayers()
 	for i := range players {
-		players[i].eventChannel <- joinReply
+		name := "(empty)"
+		if players[i] != nil {
+			name = players[i].name
+		}
+		playerListReply = fmt.Sprintf("%s %s", playerListReply, name)
 	}
+	return playerListReply
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	http.ServeFile(w, r, "spades.html")
+	if r.URL.Path == "/" {
+		http.ServeFile(w, r, "spades.html")
+		return
+	}
+	if strings.HasPrefix(r.URL.Path, "/images/") {
+		http.ServeFile(w, r, r.URL.Path[1:])
+		return
+	}
+	http.Error(w, "Not found", http.StatusNotFound)
 }
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
@@ -74,6 +107,10 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	sendChannel := make(chan string)
+	client := &client{eventChannel: sendChannel}
+	clients = append(clients, client)
+
 	go func() {
 		for {
 			_, msg, err := conn.ReadMessage()
@@ -81,26 +118,29 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 				log.Printf("Error reading message: %v", err)
 				return
 			}
-			processMessage(conn, string(msg))
+			processMessage(client, string(msg))
 		}
 	}()
+
+	go func() {
+		for message := range sendChannel {
+			if err := conn.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+				log.Printf("Error sending message %s: %v", message, err)
+			} else {
+				log.Printf("Sent message %s", message)
+			}
+		}
+	}()
+	sendChannel <- buildPlayerList()
 }
 
-func sendMessage(conn *websocket.Conn, message string) {
-	if err := conn.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
-		log.Printf("Error sending message %s: %v", message, err)
-	} else {
-		log.Printf("Sent message %s", message)
-	}
-}
-
-func processMessage(conn *websocket.Conn, message string) {
+func processMessage(client *client, message string) {
 	log.Printf("Received message: %s", message)
 
 	segments := strings.Split(message, " ")
 	switch segments[0] {
-	case "join":
-		join(conn, segments[1])
+	case "sit":
+		sit(client, segments[1])
 	}
 }
 
